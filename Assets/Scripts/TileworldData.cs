4105bf292197248b662a775492e2b4771e005afe
LucasBoe <lucas@lumotex.de>
2021-09-26 20:07:31 +0200

    Started working on tileworld renderer

--- Assets/Scripts/TileworldData.cs
+++ Assets/Scripts/TileworldData.cs
@@ -1,8 +1,97 @@
+using NaughtyAttributes;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class TileworldData : ScriptableObject
 {
-    public Vector2Int air;
+    public const int xSize = 40;
+    public const int ySize = 40;
+    public const int zSize = 10;
+
+    public int zOffset => xSize * ySize;
+    public int yOffset => xSize;
+
+    public Tile[] Tiles = new Tile[xSize * ySize * zSize];
+
+    public Tile this[int x, int y, int z]
+    {
+        get => Tiles[z * zOffset + y * yOffset + x];
+        set => Tiles[z * zOffset + y * yOffset + x] = value;
+    }
+
+    public Vector3Int this[int i]
+    {
+        get
+        {
+            int z = (int)(i / (float)zOffset);
+            i -= (int)(z * (float)zOffset);
+            int y = (int)(i / (float)yOffset);
+            int x = (int)(i % (float)yOffset);
+            return new Vector3Int( x * 2, y * 2, z * 2);
+        }
+    }
+
+    public Tile[] Test = new Tile[10];
+
+    internal Tile GetTile(int x, int y, int z)
+    {
+        return this[x, y, z];
+    }
+
+    public Vector3Int locationToSet;
+    public bool airToSet;
+    public bool visibleToSet;
+    public TileType typeToSet;
+
+    [Button]
+    public void Set()
+    {
+        if (this[locationToSet.x, locationToSet.y, locationToSet.z] == null)
+            this[locationToSet.x, locationToSet.y, locationToSet.z] = new Tile();
+
+        this[locationToSet.x, locationToSet.y, locationToSet.z].Air = airToSet;
+        this[locationToSet.x, locationToSet.y, locationToSet.z].Visible = visibleToSet;
+        this[locationToSet.x, locationToSet.y, locationToSet.z].Type = typeToSet;
+    }
+
+    [Button]
+    void Fill()
+    {
+        //Tiles = new Tile[xSize, ySize, zSize];
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int z = 0; z < zSize; z++)
+                {
+                    this[x, y, z] = new Tile();
+                }
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class Tile
+{
+    public TileType Type;
+    public bool Air;
+    public bool Visible;
+    public MeshRenderer MeshRenderer;
+
+    public static Vector3 ToVector(int x, int y, int z)
+    {
+        return new Vector3(x * 2, y * 2, z * 2);
+    }
+}
+
+public enum TileType
+{
+    Tomb,
+    Cave,
 }
--- Assets/Scripts/TileworldRenderer.cs
+++ Assets/Scripts/TileworldRenderer.cs
@@ -1,8 +1,10 @@
+using NaughtyAttributes;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
 public class TileworldRenderer : MonoBehaviour
@@ -37,6 +39,8 @@ public class TileworldRenderer : MonoBehaviour
             materials[2] = matFloor;
         }
 
+
+
         materials[4] = otherTiles.Contains(tile + Vector3Int.back * 2) ? matTransparent : matHWalls;
         materials[3] = otherTiles.Contains(tile + Vector3Int.left * 2) ? matTransparent : matVWalls;
         materials[0] = otherTiles.Contains(tile + Vector3Int.forward * 2) ? matTransparent : matHWalls;
@@ -58,7 +62,108 @@ public class TileworldRenderer : MonoBehaviour
                 list.Add(touched + Vector3Int.up * 2 * i);
             }
         }
+        Debug.LogError("d");
+
 
         CreateRoom(parent, list.ToArray());
+    }
+
+    [Button]
+    public void UpdateAllTiles()
+    {
+        for (int x = 0; x < TileworldData.xSize; x++)
+        {
+            for (int y = 0; y < TileworldData.ySize; y++)
+            {
+                for (int z = 0; z < TileworldData.zSize; z++)
+                {
+                    UpdateTile(x, y, z);
+                }
+            }
+        }
+    }
+
+    private void UpdateTile(int x, int y, int z)
+    {
+        Tile tile = world[x, y, z];
+
+
+        if (tile.Visible)
+        {
+            if (tile.MeshRenderer == null)
+                tile.MeshRenderer = Instantiate(tilePrefab, Tile.ToVector(x, y, z), Quaternion.identity, transform);
+
+            tile.MeshRenderer.gameObject.SetActive(true);
+
+            Material[] materials = tile.MeshRenderer.sharedMaterials;
+
+            bool notBelow = y == 0 || !world[x, y - 1, z].Air;
+            bool back = z == 0 || !world[x, y, z - 1].Air;
+            bool front = z == TileworldData.zSize || !world[x, y, z + 1].Air;
+            bool left = x == 0 || !world[x - 1, y, z].Air;
+            bool right = x == TileworldData.xSize || !world[x + 1, y, z].Air;
+
+            if (!notBelow)
+            {
+                materials[2] = matTransparent;
+                DestroyImmediate(tile.MeshRenderer.GetComponent<BoxCollider>());
+                y--;
+            }
+            else
+            {
+                materials[2] = matFloor;
+            }
+
+            materials[4] = back ? matHWalls : matTransparent;
+            materials[3] = left ? matVWalls : matTransparent;
+            materials[0] = front ? matHWalls : matTransparent;
+            materials[1] = right ? matVWalls : matTransparent;
+
+            tile.MeshRenderer.sharedMaterials = materials;
+        }
+        else
+        {
+            if (tile.MeshRenderer != null)
+                tile.MeshRenderer.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (world.Tiles == null)
+        {
+            Debug.Log("No Tiles...");
+            return;
+        }
+
+        /*
+        for (int x = 0; x < TileworldData.xSize; x++)
+        {
+            for (int y = 0; y < TileworldData.ySize; y++)
+            {
+                for (int z = 0; z < TileworldData.zSize; z++)
+                {
+                    Tile tile = world.GetTile(x,y,z);
+                    if (tile != null && tile.Air)
+                    {
+                        Gizmos.color = tile.Type == TileType.Tomb ? Color.yellow : Color.white;
+                        Gizmos.DrawCube(Tile.ToVector(x, y, z), Vector3.one * 2);
+                    }
+
+                    i++;
+                }
+            }
+        }
+        */
+
+        for (int i = 0; i < world.Tiles.Length; i++)
+        {
+            Tile tile = world.Tiles[i];
+            if (tile != null && tile.Air)
+            {
+                Gizmos.color = tile.Type == TileType.Tomb ? Color.yellow : Color.white;
+                Gizmos.DrawCube(world[i], Vector3.one * 2);
+            }
+        }
     }
 }
